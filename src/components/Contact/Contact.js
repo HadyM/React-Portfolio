87e00928e1330feb4ea82131f194fa9d02c95d84
Hady Mohamed <hadymohamed@pursuit.org>
2022-08-15 17:23:56 -0400

    created form in contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,67 @@
 import { Loader } from 'react-loaders';
+import { useState, useEffect } from 'react';
+import AnimatedLettering from '../AnimatedLettering/AnimatedLettering';
 import './Contact.scss';
 
 const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 3000);
+  }, []);
+
   return (
     <>
-      <div></div>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLettering
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'M', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in job or freelance opportunities - especially
+            ambitious or large projects. However, if you have other requests or
+            questions, please don't hesitate to contact me using the form below.
+            I look forward to hearing from you!
+          </p>
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    name="subject"
+                    placeholder="Subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea name="message" placeholder="Message" required />
+                </li>
+                <li>
+                  <input className="flat-button" type="submit" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
       <Loader type="pacman" />
     </>
   );
