263d4987385988c4ae6d9e0636d5f1b02ad8735d
Hady Mohamed <hadymohamed@pursuit.org>
2023-01-09 19:03:47 -0500

    started responsive build, changed layout of sidebar and added a hamburger menu for mobile view

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -5,6 +6,8 @@ import {
   faEnvelope,
   faHome,
   faUser,
+  faBars,
+  faClose,
 } from '@fortawesome/free-solid-svg-icons';
 import {
   faGithub,
@@ -16,78 +19,95 @@ import Logo from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
 import './Sidebar.scss';
 
-const Sidebar = () => (
-  <>
-    <div className="nav-bar">
-      <Link className="logo" to="/">
-        <img src={Logo} alt="logo" />
-        <img className="sub-logo" src={LogoSubtitle} alt="devhadym" />
-      </Link>
-      <nav>
-        <NavLink exact="true" activeclassname="active" to="/">
-          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-        </NavLink>
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          className="about-link"
-          to="/about"
-        >
-          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-        </NavLink>
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          className="projects-link"
-          to="/projects"
-        >
-          <FontAwesomeIcon icon={faBarsProgress} color="#4d4d4e" />
-        </NavLink>
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          className="contact-link"
-          to="/contact"
-        >
-          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-        </NavLink>
-      </nav>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.linkedin.com/in/hady-mohamed-709307187/"
-          >
-            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel="noreferrer" href="https://github.com/HadyM">
-            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://twitter.com/Hady_Mohamed_87"
-          >
-            <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.reddit.com/user/PhoenixNile"
-          >
-            <FontAwesomeIcon icon={faReddit} color="#4d4d4e" />
-          </a>
-        </li>
-      </ul>
-    </div>
-  </>
-);
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
 
+  return (
+    <>
+      <div className="nav-bar">
+        <Link className="logo" to="/">
+          <img src={Logo} alt="logo" />
+          <img className="sub-logo" src={LogoSubtitle} alt="devhadym" />
+        </Link>
+        <nav className={showNav ? 'mobile-show' : ''}>
+          <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="about-link"
+            to="/about"
+          >
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="projects-link"
+            to="/projects"
+          >
+            <FontAwesomeIcon icon={faBarsProgress} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="contact-link"
+            to="/contact"
+          >
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          </NavLink>
+          <FontAwesomeIcon
+            onClick={() => setShowNav(false)}
+            icon={faClose}
+            color="#ff000099"
+            size="3x"
+            className="close-icon"
+          />
+        </nav>
+        <ul>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/hady-mohamed-709307187/"
+            >
+              <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noreferrer" href="https://github.com/HadyM">
+              <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://twitter.com/Hady_Mohamed_87"
+            >
+              <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.reddit.com/user/PhoenixNile"
+            >
+              <FontAwesomeIcon icon={faReddit} color="#4d4d4e" />
+            </a>
+          </li>
+        </ul>
+        <FontAwesomeIcon
+          onClick={() => setShowNav(true)}
+          icon={faBars}
+          color="#ff000099"
+          size="3x"
+          className="hamburger-icon"
+        />
+      </div>
+    </>
+  );
+};
 export default Sidebar;
